12042c6c56be7db6aec9878a08a57d9245a9d7ad
magicprancer <magicprancer3@outlook.com>
2025-11-24 14:21:26 -0600

    Create AiUtils.java

---
+++ src/main/java/AiUtils.java
@@ -0,0 +1,7 @@
+package com.example.aiplugin;
+
+import org.bukkit.command.CommandSender;
+
+public class AIUtils {
+    public static void send(CommandSender s, String msg) { s.sendMessage(msg); }
+}
